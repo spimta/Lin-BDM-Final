3de7b35610266c43425af57f79b4ecb129e85e38
Yi Lin <spimtalin@gmail.com>
2021-12-05 17:46:31 -0500

    function s

--- HW4.py
+++ HW4.py
@@ -57,14 +57,20 @@ def main(sc):
     df_weekly = df_weekly.withColumnRenamed('_c0', 'placekey').withColumnRenamed('_c12', 'date_range_start').withColumnRenamed('_c16', 'visits_by_day')
     df_main = df_core_place.join(df_weekly.alias('weekly'), df_core_place.placekey == df_weekly.placekey, 'inner').select("weekly.placekey", "date_range_start", "visits_by_day", "naics_code")
 
+    
 
-    df_main = df_main.select('placekey', F.explode(udfExpand('date_range_start', 'visits_by_day')).alias('date', 'visits'), 'naics_code').filter((df_main.date >= datetime.date(2019, 1, 1)) & (df_main.date <= datetime.date(2020, 12, 31)))
+    df_main = df_main.select('placekey', F.explode(udfExpand('date_range_start', 'visits_by_day')).alias('date', 'visits'), 'naics_code')
+    df_main = df_main.filter((df_main.date >= datetime.date(2019, 1, 1)) & (df_main.date <= datetime.date(2020, 12, 31)))
     df_median = df_main.groupBy('naics_code', 'date').agg(F.expr('percentile(visits, array(0.5))')[0].alias('median'))
     df_stddev = df_main.groupBy('naics_code', 'date').agg({'visits': 'stddev'}).withColumnRenamed('stddev(visits)', 'stddev')
     df_main = df_median.alias('a').join(df_stddev, (df_median.naics_code == df_stddev.naics_code) & (df_median.date == df_stddev.date), 'outer').select('a.naics_code', 'a.date', 'median', 'stddev')
 
+    
 
-    df_main = df_main.withColumn('low', udfLow('median', 'stddev')).withColumn('high', df_main.median + df_main.stddev).drop(df_main.stddev).withColumn('year', F.year(df_main.date)).withColumn("date", udfChangeYear('date'))
+    df_main = df_main.withColumn('low', udfLow('median', 'stddev')).withColumn('high', df_main.median + df_main.stddev).withColumn('year', F.year(df_main.date)).drop(df_main.stddev)
+
+    
+    df_main = df_main.withColumn("date", udfChangeYear('date'))
 
     for catagory_name, naics_codes in catagories.items():
         catagory_name = catagory_name.replace(" ", "_").lower()
