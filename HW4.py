9cd4093a00711f92c895b89b273a7a18ca152acc
Yi Lin <spimtalin@gmail.com>
2021-12-05 20:36:26 -0500

    fix

--- HW4.py
+++ HW4.py
@@ -68,7 +68,7 @@ for catagory_name, naics_codes in catagories.items():
     df_main = df_core_place2.join(df_weekly.alias('weekly'), df_core_place2.placekey == df_weekly.placekey, 'inner').select("weekly.placekey", "date_range_start", "visits_by_day", "naics_code")
     df_main = df_main.select('placekey', F.explode(udfExpand('date_range_start', 'visits_by_day')).alias('date', 'visits'), 'naics_code')
     df_main = df_main.filter((df_main.date >= datetime.date(2019, 1, 1)) & (df_main.date <= datetime.date(2020, 12, 31)))
-    df_median = df_main.groupBy('date').agg(F.expr('percentile(visits, array(0.5))')[0].alias('median'), F.stddev('visits').alias('stddev'))
+    df_main = df_main.groupBy('date').agg(F.expr('percentile(visits, array(0.5))')[0].alias('median'), F.stddev('visits').alias('stddev'))
     #df_main = df_median.alias('a').join(df_stddev, (df_median.date == df_stddev.date), 'outer').select('a.date', 'median', 'stddev')
 
     df_main = df_main.withColumn('low', udfLow('median', 'stddev')).withColumn('high', df_main.median + df_main.stddev).withColumn('year', F.year(df_main.date)).drop(df_main.stddev)
