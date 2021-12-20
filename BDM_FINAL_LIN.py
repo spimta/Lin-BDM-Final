2c10c38936c7ffb3e2242bff37bdb36a7fa39f9f
Yi Lin <spimtalin@gmail.com>
2021-12-20 05:37:56 -0500

    update

--- BDM_FINAL_LIN.py
+++ BDM_FINAL_LIN.py
@@ -107,11 +107,12 @@ def main(sc, spark):
 
     for filename, group_num in GROUP.items():
         header_data = [("year", "date", "median", "low", "high")]
-        spark.createDataFrame(data=header_data).write.csv(f'{OUTPUT_PREFIX}/{filename}', mode='overwrite', header=False)
+        header_df = spark.createDataFrame(data=header_data)
         df.filter(df.group == group_num) \
-            .drop('group') \
-            .coalesce(50) \
-            .write.csv(f'{OUTPUT_PREFIX}/{filename}', mode='append', header=False)
+            .drop('group')
+
+        df = header_df.union(df)
+        df.coalesce(50).write.csv(f'{OUTPUT_PREFIX}/{filename}', mode='overwrite', header=False)
 
 
 if __name__=='__main__':
